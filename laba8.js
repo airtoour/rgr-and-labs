120fdbe632db0513221e5d3e1190c1355bb0de9b
Arthur Weitz <artur.gips@mail.ru>
2022-12-20 18:04:41 +0700

    Количество дней с даты рождения

--- laba8.js
+++ laba8.js
@@ -4,4 +4,14 @@ function showDate(params) {
     out.innerHTML = today.toLocaleDateString();
 }
 
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount); // Округление в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
+
 window.addEventListener('load', showDate);
